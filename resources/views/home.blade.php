57cb18968fe8de44b1477fd5ed313429aced9f06
yamone178 <yamonemyatmoe1592@gmail.com>
2022-08-12 14:41:38 +0630

    to revert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -15,8 +14,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
-                </div>
-            </div>
+
         </div>
     </div>
 </div>
